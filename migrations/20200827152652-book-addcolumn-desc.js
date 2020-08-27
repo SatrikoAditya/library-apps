7f55b5a24c21abf21d6fa7c497a2d2e11232132e
sy-permana <suryapermana996@gmail.com>
2020-08-28 00:22:01 +0800

    models user, books and bcrypt

---
+++ migrations/20200827152652-book-addcolumn-desc.js
@@ -0,0 +1,23 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    return queryInterface.addColumn('Books', 'desc', Sequelize.TEXT);
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    return queryInterface.removeColumn('Books', 'desc');
+  }
+};
